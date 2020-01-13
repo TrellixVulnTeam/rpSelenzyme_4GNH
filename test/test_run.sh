16caf958aae045ac75284b6f70a3c7f3208b6239
mdulac <melchior.dulac@inra.fr>
2020-01-13 14:37:18 +0100

    added test

--- test/test_run.sh
+++ test/test_run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-docker run -p 5000:5000 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' brsynth/selenzyme
+rm -f test_output.tar
+docker run -d -p 5000:5000 --name test_rpSelenzyme -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' brsynth/selenzyme
 sleep 30
 python tool_rpSelenzyme.py -inputTar test_input.tar -outputTar test_output.tar -pathway_id rp_pathway -server_url http://0.0.0.0:5000/REST
-docker kill test_rpExtractSink
-docker rm test_rpExtractSink
+docker kill test_rpSelenzyme
+docker rm test_rpSelenzyme
