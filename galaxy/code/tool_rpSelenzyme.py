6f3052fc50421a9c7fabd8522ec4f75ebb0a4194
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 22:18:57 +0000

    Adding tarfile member sanitization to extractall()

--- galaxy/code/tool_rpSelenzyme.py
+++ galaxy/code/tool_rpSelenzyme.py
@@ -91,7 +91,26 @@ if __name__ == "__main__":
                                          params.rxntype,
                                          params.min_aa_length)
             with tarfile.open(output_tar) as outTar:
-                outTar.extractall(tmpOutputFolder)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(outTar, tmpOutputFolder)
             out_file = glob.glob(tmpOutputFolder+'/*.xml')
             if len(out_file)>1:
                 logging.error('There are more than one output file...')
